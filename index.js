a879f28781d6a13a169093725104154dc1e73215
Mayanklna <mp410081@gmail.com>
2021-02-26 17:56:36 +0530

    some cors

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const floodsroutes=require('./routes/flooddata')
 const app=express();
 require('dotenv').config();
  
- 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept');
+    next();
+  }); 
 app.use(bodyParser.json({limit:'50mb'}));
 app.use(bodyParser.urlencoded({
   extended: true
